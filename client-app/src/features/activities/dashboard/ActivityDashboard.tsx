e4ddbed6cc7eaa7df907970e73f4cf89beaf7a01
Malini <malinia@deloitte.com>
2023-03-25 14:53:09 +0530

    end of section 7

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,57 +1,34 @@
-import React from 'react'
-import { Grid, List } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import { observer } from 'mobx-react-lite';
+import { useEffect } from 'react';
+import { Grid } from 'semantic-ui-react'
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props {
-    activities: Activity[];
-    selectedActivity: Activity | undefined;
-    selectActivity: (id: string) => void;
-    cancelSelectActivity: () => void;
-    editMode: boolean;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
+export default observer(function ActivityDashboard() {
 
-export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting}: Props) {
-  return (
-    <Grid>
-        <Grid.Column width='10'>
-            {/* <List>
-                {activities.map(activity => (
-                    <List.Item key={activity.id}>
-                        {activity.title}
-                    </List.Item>
-                ))}
-            </List> */}
-            <ActivityList
-              activities={activities}
-              selectActivity={selectActivity}
-              deleteActivity={deleteActivity}
-              submitting={false}
-            />
-        </Grid.Column>
-        <Grid.Column width='6'>
-            {selectedActivity && !editMode &&
-            <ActivityDetails
-                activity={selectedActivity}
-                cancelSelectActivity={cancelSelectActivity}
-                openForm={openForm}
-            />}
-            {editMode &&
-            <ActivityForm
-                  closeForm={closeForm}
-                  activity={selectedActivity}
-                  submitting={submitting}
-                  createOrEdit={function (activity: Activity): void {
-                      throw new Error('Function not implemented.');
-                  } } />}
-        </Grid.Column>
-    </Grid>
+    const {activityStore} = useStore();
+    const {selectedActivity, editMode} = activityStore;
+
+    useEffect(() => {
+      activityStore.loadActivities();
+    }, [activityStore])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+  
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && !editMode &&
+                <ActivityDetails />}
+                {editMode &&
+                <ActivityForm />}
+            </Grid.Column>
+        </Grid>
   )
-}
+})
